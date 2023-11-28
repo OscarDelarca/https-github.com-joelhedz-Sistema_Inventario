22aeaddd88aa72241cd5ec937fce3ed8ab4f7da1
Xlegenda Lan <oscardaviddelarca4@hotmail.com>
2023-11-27 21:02:02 -0600

    a

--- Sistema_Inventario/Formularios/FrmLogin.Designer.cs
+++ Sistema_Inventario/Formularios/FrmLogin.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(293, 116);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
             // 
             // FrmLogin
             // 
             AutoScaleMode = AutoScaleMode.Inherit;
             ClientSize = new Size(1000, 600);
             ControlBox = false;
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(4);
             Name = "FrmLogin";
@@ -42,8 +53,11 @@
             Text = "FrmLogin";
             Load += FrmLogin_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
